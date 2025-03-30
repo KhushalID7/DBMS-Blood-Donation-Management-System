00c34d2a654c2289d3b9d27de921451653f574ed
Khushal Shroff <23bcp137@sot.pdpu.ac.in>
2025-03-30 11:12:48 +0530

    Delete sql directory

--- sql/Donation event.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE DonationEvent (
-    EventID INT PRIMARY KEY,
-    EventName VARCHAR(255) NOT NULL,
-    Location VARCHAR(255) NOT NULL,
-    EventDate DATE NOT NULL,
-    Organizer VARCHAR(255) NOT NULL
-);
